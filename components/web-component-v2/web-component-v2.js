90f98c8c52b41635b13072735f5df4d23a4dbb91
alessandro miele <alessandro.miele@decathlon.com>
2024-10-03 16:04:16 +0200

    feat: fix propr name, secon version

--- components/web-component-v2/web-component-v2.js
+++ components/web-component-v2/web-component-v2.js
@@ -1,6 +1,6 @@
 //component name
 const componentPath = "components";
-const componentName = "web-component"
+const componentName = "web-component-v2"
 
 //reactive props
 const propx = {
@@ -15,7 +15,7 @@ let rex = {
 const logix = (document) => {
   const inputText = document.querySelector("#input-text");
   const inputButton = document.querySelector("#input-button");
-  inputButton.onclick = () => { rex.textInput = inputText.value;};
+  inputButton.onclick = () => { rex.textInput = inputText.value; };
 }
 
 // library functions
@@ -27,37 +27,48 @@ class WebComponent extends HTMLElement {
   constructor() {
     super();
     let innerShadowDom = this.shadowDom;
-    innerShadowDom.innerHTML = compileHtml();
+    innerShadowDom.innerHTML = html;
     //listen for rex change
     rex = new Proxy(rex, {
       set(target, property, value) {
         target[property] = value;
-        innerShadowDom.innerHTML = compileHtml();
-        //rehydrate
-        logix(innerShadowDom);
+        updatePropxAndRex(this.shadowDom, {
+          type: "rex",
+          id: property,
+          value: value
+        });
         return true;
       }
     })
   }
 
+  //element is loaded
+  connectedCallback() {
+    bindPropAndRex(this.shadowDom, propx, rex)
+    logix(this.shadowDom);
+  }
+
   //listen for props change, update html
   attributeChangedCallback(propName, oldValue, newValue) {
     propx[propName] = newValue;
-    this.shadowDom.innerHTML = compileHtml();
-    //rehydrate
-    logix(this.shadowDom);
+    updatePropxAndRex(this.shadowDom, {
+      type: "propx",
+      id: propName,
+      value: newValue
+    });
   }
 
 }
 let html = await fetch(`${componentPath}/${componentName}/${componentName}.html`).then(res => res.text());
-const compileHtml = () => {
-  let compiledHtml = html;
+const bindPropAndRex = (shadowDom, propx, rex) => {
+  const allElement = shadowDom.querySelectorAll("*");
+  allElement.forEach(el => console.log(el));
   Object.keys(propx).forEach(key => {
-    compiledHtml = compiledHtml.replace(`propx.${key}`, propx[key])
+
   });
-  Object.keys(rex).forEach(key => {
-    compiledHtml = compiledHtml.replace(`rex.${key}`, rex[key])
-  });
-  return compiledHtml;
+
+}
+const updatePropxAndRex = (shadowDom, data) => {
+
 }
 customElements.define(componentName, WebComponent);
