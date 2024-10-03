463ccf9154b6a79e0957505f85f3f80c0595280e
alessandro miele <alessandro.miele@decathlon.com>
2024-10-03 17:50:51 +0200

    feat: added slot

---
+++ components/slot-component/slot-component.js
@@ -0,0 +1,76 @@
+//component name
+const componentPath = "components";
+const componentName = "slot-component"
+
+//reactive props
+const propx = {
+  title: ""
+}
+//reactive var
+let rex = {
+
+}
+//component logic
+const logix = (document) => {
+
+}
+
+// library functions
+export class WebComponent extends HTMLElement {
+  static observedAttributes = Object.keys(propx);
+  shadowDom = this.attachShadow({ mode: "open" });
+
+  //init component
+  constructor() {
+    super();
+    //inject first html
+    this.shadowDom.innerHTML = bindReactiveText(html, propx, rex);
+  }
+
+  //element is loaded
+  connectedCallback() {
+    const shadowDom = this.shadowDom;
+    //listen for rex change, update html text
+    rex = new Proxy(rex, {
+      set(target, property, value) {
+        target[property] = value;
+        updateReactiveText(shadowDom, {
+          type: "rex",
+          id: property,
+          value: value
+        });
+        return true;
+      }
+    });
+    logix(shadowDom);
+  }
+
+  //listen for props change, update html text
+  attributeChangedCallback(propName, oldValue, newValue) {
+    propx[propName] = newValue;
+    updateReactiveText(this.shadowDom, {
+      type: "propx",
+      id: propName,
+      value: newValue
+    });
+  }
+}
+let html = await fetch(`${componentPath}/${componentName}/${componentName}.html`).then(res => res.text());
+//bind propx to text element
+const bindReactiveText = (html, propx, rex) => {
+  let compiledHtml = html;
+  Object.keys(propx).forEach(key => {
+    compiledHtml = compiledHtml.replace(`propx.${key}`, `<span propx-${key}>${propx[key]}</span>`)
+  });
+  Object.keys(rex).forEach(key => {
+    compiledHtml = compiledHtml.replace(`rex.${key}`, `<span rex-${key}>${rex[key]}</span>`)
+  });
+  return compiledHtml;
+}
+//update text
+const updateReactiveText = (shadowDom, data) => {
+  shadowDom.querySelectorAll(`[${data.type}-${data.id}]`).forEach(el => {
+    el.textContent = data.value;
+  })
+}
+customElements.define(componentName, WebComponent);
--- readme.md
+++ readme.md
@@ -5,3 +5,9 @@ This is a test project aimed at bringing into a web component a minimal set of f
 
 ### how to run it
 It's plain vanillaJS, so you just need to clone the repo and use the Live Server plugin for VS Code to serve ```index.html``` from an HTTP server.
+
+### what you can do rn?
+- You can have reactive props (only for plain text)
+- You can have reactive variables (only for plain text)
+- You can split Js locig and html/css for every component
+- You can work with slots
